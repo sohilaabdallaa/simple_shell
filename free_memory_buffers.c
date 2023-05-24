88ede5c14fe962b89fc1449ab33eacde73d73952
nada <nada151adel@gmail.com>
2023-05-23 23:30:32 -0700

    file 7

---
+++ free_memory_buffers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * free_memory_buffers - frees buffers
+ * @buf: buffer to be freed
+ * Return: no return
+ */
+
+void free_memory_buffers(char **buf)
+{
+	int n = 0;
+
+	if (!buf || buf == NULL)
+		return;
+	while (buf[n])
+	{
+		free(buf[n]);
+		n++;
+	}
+	free(buf);
+
+}
