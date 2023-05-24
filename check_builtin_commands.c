dcb4f35087393e6b476622ff3741299094824a03
nada <nada151adel@gmail.com>
2023-05-23 23:24:52 -0700

    file two

---
+++ check_builtin_commands.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * check_builtin_commands- checks to see weather its a built in function
+ * @c: tokenized user input
+ * @buf: line drived fromgetline function
+ * Return: 1 if c excuted 0 if c is not executed
+ */
+
+int check_builtin_commands(char **c, char *buf)
+{
+	if (handle_builtin_command(c, buf))
+		return (1);
+	else if (**c == '/')
+	{
+		execution(c[0], c);
+		return (1);
+	}
+	return (0);
+}
+
